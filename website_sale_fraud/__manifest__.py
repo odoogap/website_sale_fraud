3d8e8da8c30663d7c1118dcc37492517197e6938
Diogo Duarte <dduarte@odoogap.com>
2024-05-31 14:34:06 +0100

    [IMP] initial commit

---
+++ website_sale_fraud/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'eCommerce - Fraud Detection',
+    'category': 'Website/Website',
+    'sequence': 50,
+    'summary': 'Detect fraudulent orders',
+    'website': 'https://www.erpgap.com/ecommerce/',
+    'version': '1.1',
+    'depends': ['website_sale'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
